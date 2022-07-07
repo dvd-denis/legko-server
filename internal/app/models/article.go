57872d99ae71c5ce11782ad341d24caea2572d66
Denis <pikuldenis9@gmail.com>
2022-07-07 11:36:20 +0600

    Add json param

--- internal/app/models/article.go
+++ internal/app/models/article.go
@@ -1,12 +1,12 @@
 package models
 
 type Article struct {
-	Id       int    `db:"id"`
-	Title    string `db:"title"`
-	IconName string `db:"icon_name"`
-	Icon     string `db:"icon"`
-	Url      string `db:"url"`
-	Color    string `db:"color"`
+	Id       int    `db:"id" json:"id"`
+	Title    string `db:"title" json:"title"`
+	IconName string `db:"icon_name" json:"icon_name"`
+	Icon     string `db:"icon" json:"icon"`
+	Url      string `db:"url" json:"url"`
+	Color    string `db:"color" json:"color"`
 	Wifi     bool   `db:"wifi" json:"wifi"`
 	Question bool   `db:"question" json:"-"`
 }
